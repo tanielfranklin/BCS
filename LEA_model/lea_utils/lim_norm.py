7a4abe90e2606697b10682928f13a4f26054bec0
taniel <tanielfranklin@gmail.com>
2022-01-25 23:23:36 -0300

    Metodo para valores característicos de normalizar

---
+++ LEA_model/lea_utils/lim_norm.py
@@ -0,0 +1,22 @@
+import numpy as np
+def norm_values():
+    #========================================
+    # Limites das variáveis para normalização LEA
+    #========================================
+    flim=[35,65]
+    zlim=[5,70]
+    pbhlim=(1e5,2.5e5) 
+    pwhlim=(2e5,11e5) 
+    qlim=(0.1/3600,5/3600)
+    def Lim_c(x):
+        return x[1]-x[0]
+    pbc=Lim_c(pbhlim)
+    pwc=Lim_c(pwhlim)
+    qc=Lim_c(qlim)
+    pbmin=pbhlim[0]
+    pwmin=pwhlim[0]
+    qmin=qlim[0]
+    #xss = np.float32(np.array([8311024.82175957,2990109.06207437,0.00995042241351780]))
+    x0=np.array([pbmin,pwmin,qmin])#,0,0])
+    xc=np.array([pbc,pwc,qc])#,1,1])
+    return xc,x0
